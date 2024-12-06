8225dc1ca363f54dc38011c8a0807ec6ee360ae9
calebewerneckcouto <calebewerneck@hotmail.com>
2024-12-06 14:30:49 -0300

    importante

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,17 +5,29 @@ import entities.Client;
 public class Program {
 
 	public static void main(String[] args) {
+
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c3 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Alex", "alex@gmail.com");
+
+		String s1 = new String("test");
+		String s2 = new String("test");//da false
 		
-		Client c1 = new Client("Maria","maria@gmail.com");
-		Client c3 = new Client("Maria","maria@gmail.com");
-		Client c2 = new Client("Alex","alex@gmail.com");
-	
-	     System.out.println(c1.hashCode());
-	     System.out.println(c2.hashCode());
-	     System.out.println(c3.hashCode());
-	     
-	     System.out.println(c1.equals(c2));
-	     System.out.println(c1.equals(c3));
+		
+		String s3 = "test";//da true
+		String s4 = "test";
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c3.hashCode());
+
+		System.out.println(c1.equals(c2));
+		System.out.println(c1.equals(c3));
+		
+		
+		System.out.println(c1 == c2);
+		System.out.println(s1 == s2);
+		System.out.println(s3 == s4);
 	}
 
 }
